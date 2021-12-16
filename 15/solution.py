5b0247c0c0dbd8875a7ad3b1ff2186e824b56cb8
Dan <dan.weatherill@cantab.net>
2021-12-16 12:14:57 +0000

    solution part 2 day 15

--- 15/solution.py
+++ 15/solution.py
@@ -38,15 +38,33 @@ def path_total_risk(path, graph):
         total_risk += graph.nodes[node]["risk"]
     return total_risk
 
-if __name__ == "__main__":
-    grid = load_grid("example_input.txt")
-    grid = load_grid("input.txt")
-    graph = construct_graph(grid)
+def expand_grid(grid: np.ndarray) -> np.ndarray:
+    #expand down 5 times
+    column = np.hstack( (grid, grid+1, grid+2, grid+3, grid+4))
+    full_grid = np.vstack((column, column+1, column+2, column+3, column+4))
+    cond = full_grid > 9
+    full_grid[cond] = full_grid[cond]%9
+    return full_grid
+
+def shortest_path_find(grid, graph):
     tgt_node = (grid.shape[0]-1, grid.shape[1]-1)
     weightfun = lambda fr, to, attrs: graph.nodes[to]["risk"]
-
     paths = nx.shortest_simple_paths(graph, source=(0,0), target=tgt_node, weight=weightfun)
     shortest_path = next(paths)
+    return shortest_path
+
+if __name__ == "__main__":
+    #grid = load_grid("example_input.txt")
+    grid = load_grid("input.txt")
+    graph = construct_graph(grid)
+    shortest_path = shortest_path_find(grid, graph)
     print("shortest path : %s" % shortest_path)
     risk = path_total_risk(shortest_path, graph)
     print("risk of shortest path: %d" % risk)
+
+    expanded_grid = expand_grid(grid)
+    expanded_graph = construct_graph(expanded_grid)
+    shortest_expanded_path = shortest_path_find(expanded_grid, expanded_graph)
+
+    expanded_risk = path_total_risk(shortest_expanded_path, expanded_graph)
+    print("risk of shortest expanded path: %d" % expanded_risk)
