dea29cdd3832d7b11bfa674fe684ff4632a89fdc
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2021-12-17 10:58:59 +0000

    WIP part 2

--- 17/main.cpp
+++ 17/main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include <tuple>
 #include <cmath>
+#include <vector>
+#include <set>
 
 using std::tuple;
 
 const std::tuple<int, int> example_limits_y = {-10, -5};
 const std::tuple<int, int> limits_y = {-171, -136};
 
+const std::tuple<int,int> example_limits_x = {20, 30};
+const std::tuple<int, int> limits_x = {60,94};
+
 using std::cout;
 using std::endl;
 
-std::tuple<double, double> solve_quadratic(int vely0, int y)
-{
-  double determinant = std::sqrt(vely0 * vely0/4 +  y);
-  return {vely0/2 + determinant, vely0/2 - determinant};
-}
 
 std::tuple<double, double> solve_quadratic2(int vely0, int y)
 {
   auto f = (vely0 + 0.5);
   auto det = std::sqrt( f*f - 2*y);
   return {f + det, f-det};
-
 }
 
 double velocity_lowerlim_determinant(int ymin)
@@ -35,8 +34,10 @@ int y_n(int v, int n)
 }
 
 
-int main(int argc, char** argv)
+void part1() 
 {
+  // {{{ 
+
   //auto test = example_limits_y;
   auto test = limits_y;
   auto v_lowlim = velocity_lowerlim_determinant(std::get<0>(test));
@@ -79,5 +80,84 @@ int main(int argc, char** argv)
   cout << "-----------------------" << endl;
   for(int i=nmax-1; i < nmax+3; i++)
     cout <<"i: " << i << " maxheight: " << y_n(tryvel, i) << endl;
+
+  
+};
+
+// }}}
+
+template<typename Func>
+std::vector<int>  search_ys(const std::tuple<int,int>& test, Func&& fun)
+{
+  int thisvel = 0;
+  int targetn = 0;
+  double diff = 2.0;
+  
+  std::vector<int> valid_ys;
+  while(diff > 0.001)
+    {
+      auto [t1, t2] = solve_quadratic2(thisvel, std::get<0>(test));
+      auto [t3, t4] = solve_quadratic2(thisvel, std::get<1>(test));
+      diff = std::abs(t1 - t3);
+      auto cond = std::abs(std::floor(t1) - std::floor(t3));
+      if( cond >= 1)
+	{
+	  valid_ys.push_back(thisvel);
+	}
+      
+      fun(thisvel);
+    }
+  return valid_ys;
+}
+
+auto incr = [](int& v) { v++;};
+auto decr = [](int& v) { v--;};
+
+
+int x_n(int u, int n)
+{
+  return n < u ? n*u - n*(n-1)/2 : n*u - u*(u-1)/2;
+}
+
+int x_max(int u)
+{
+  return n*u - u*(u-1)/2;
+}
+
+template<typename Func>
+std::vector<int> search_xs(const std::tuple<int,int>& test, Func&& fun)
+{
+  
+
+}
+
+
+int main(int argc, char** argv)
+{
+  auto test = example_limits_y;
+  //auto test = limits_y;
+  int tryvel = 0;
+  int thisvel = tryvel;
+  int targetn = 0;
+  double diff = 2.0;
+
+  std::vector<int> valid_ys = search_ys(test, incr);
+
+  std::vector<int> vy2s = search_ys(test, decr);
+
+  std::copy(vy2s.begin(), vy2s.end(), std::back_insert_iterator(valid_ys));
+
+  std::set<int> unique_ys(valid_ys.begin(), valid_ys.end());
+  cout << "valid ys:" << endl;
+  for(auto& y : unique_ys)
+    cout << y << ", ";
+  cout << endl;
+
+
+  cout << "-----" << endl;
+  for(int i=0; i < 10; i++)
+    {
+      cout << "i: " << i << " x: " << x_n(7,i) << endl;
+    }
   
 }
