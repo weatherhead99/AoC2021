1bb2c6796a21c02841f97d12202ba0551fc97a10
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2021-12-17 09:50:51 +0000

    part 1 solution

--- 17/main.cpp
+++ 17/main.cpp
@@ -18,8 +18,9 @@ std::tuple<double, double> solve_quadratic(int vely0, int y)
 
 std::tuple<double, double> solve_quadratic2(int vely0, int y)
 {
-  double det = std::sqrt(4*vely0*vely0 + 4*vely0 - 8*y + 1);
-  return {-0.5*(det)+vely0+0.5, 0.5*(det + 2*vely0+1)};
+  auto f = (vely0 + 0.5);
+  auto det = std::sqrt( f*f - 2*y);
+  return {f + det, f-det};
 
 }
 
@@ -36,36 +37,47 @@ int y_n(int v, int n)
 
 int main(int argc, char** argv)
 {
-  auto test = example_limits_y;
-  //auto test = limits_y;
+  //auto test = example_limits_y;
+  auto test = limits_y;
   auto v_lowlim = velocity_lowerlim_determinant(std::get<0>(test));
   cout << "minimum valid velocity: " << v_lowlim << endl;
-
+  cout << "maximum valid velocity: " <<velocity_lowerlim_determinant(std::get<1>(test));
   int tryvel = std::ceil(v_lowlim);
-  
-  while(true)
+  int thisvel = tryvel;
+  int targetn = 0;
+  double diff = 2.0;
+  while(diff > 0.1)
     {
-      cout << "velocity: " << tryvel << endl;
-      auto [t1, t2] = solve_quadratic(tryvel, std::get<0>(test));
-      auto [t3, t4] = solve_quadratic(tryvel, std::get<1>(test));
-      cout << "t1: " << t1 << " t2: " << t2 << "t3: " << t3 << " t4: " << t4 << endl;
+      cout << "velocity: " << thisvel << endl;
+      auto [t1, t2] = solve_quadratic2(thisvel, std::get<0>(test));
+      auto [t3, t4] = solve_quadratic2(thisvel, std::get<1>(test));
+      cout << "t1: " << t1 << " t3: " << t3  << endl;
+      diff = std::abs(t1 - t3);
+      cout << "diff: " << diff << endl;
       auto cond = std::abs(std::floor(t1) - std::floor(t3));
-      cout << "yn:" << y_n(std::floor(tryvel), std::floor(t1)) << endl;;
-      if( std::abs(std::floor(t1) - std::floor(t3))< 1)
-	break;
+      if( cond >= 1)
+	{
+	  tryvel = thisvel;
+	  auto [tn1, tn2] = solve_quadratic2(tryvel, std::get<1>(test));
+	  targetn = std::ceil(tn1);
+	}
       
-      tryvel += 1.;
+      thisvel += 1.;
+
+      
     }
 
   cout << "maximum valid y velocity is: " << tryvel << endl;
   auto nmax = std::round(tryvel + 0.25);
   cout << "nmax: " << nmax << endl;
   cout << "maximum height is: " << y_n(tryvel, nmax) << endl;
-  cout << y_n(tryvel, 44) << ", " << y_n(tryvel, 45) << endl;
+  cout << "targetn: " << targetn << endl;
 
-  auto [t1, t2] = solve_quadratic2(9, 15);
-  cout << "test solve: " << t1 << ", " << t2 << endl;
-  for(int i=0; i < nmax+20; i++)
+  for(int i=targetn-3; i < targetn+3; i++)
+    cout << "n: " << i << " y: "<< y_n(tryvel, i) << endl;
+
+  cout << "-----------------------" << endl;
+  for(int i=nmax-1; i < nmax+3; i++)
     cout <<"i: " << i << " maxheight: " << y_n(tryvel, i) << endl;
   
 }
