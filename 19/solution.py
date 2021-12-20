644d962d4cfb4e0fb882838bcf2024e4324fed30
Dan <dan.weatherill@cantab.net>
2021-12-20 00:48:54 +0000

    almost working for the example solution except scanner #4

--- 19/solution.py
+++ 19/solution.py
@@ -11,6 +11,10 @@ from typing import List
 from scipy.spatial.transform import Rotation
 from itertools import combinations
 from scipy.spatial import KDTree
+from scipy.linalg import svd
+
+import matplotlib.pyplot as plt
+
 
 def read_scanners(fname: str):
     scanners = []
@@ -43,30 +47,58 @@ def register_points(coords1, coords2, n_neighbours):
         if tuple(c) in registry:
             continue
 
-        neighdist, neighind = tree1.query(c,k=n_neighbours, p=2)
+        neighdist, neighind = tree1.query(c,k=n_neighbours, p=1)
         for c2 in coords2:
             if tuple(c2) in registry.values():
                 continue
-            nd2, ni2 = tree2.query(c2, k=n_neighbours, p=2)
+            nd2, ni2 = tree2.query(c2, k=n_neighbours, p=1)
             if np.all(neighdist == nd2):
                 registry[tuple(c)] = tuple(c2)
     return registry
 
 
+def get_rotation_translation(registered_points, pr=False):
+    pts_from = np.vstack(list(registered_points.keys()))
+    pts_to = np.vstack(list(registered_points.values()))
+
+    cent_from = np.matrix((np.sum(pts_from, axis=0) / pts_from.shape[0]))
+    cent_to = np.matrix((np.sum(pts_to, axis=0) / pts_to.shape[0]))
+
+    if(pr):
+        print(cent_from)
+        print(cent_to)
+
+    #find rotation via SVD
+    R, err = Rotation.align_vectors(pts_from - cent_from, pts_to-cent_to)
+    print("registration error: %2.2f" % err)
+
+    #find translation
+    t = cent_to - R.apply(cent_from)
+
+    if(pr):
+        print(R.as_matrix())
+        print(err)
+        print(t)
+    return R, t
 
 
 if __name__ == "__main__":
     scanners = read_scanners("example_input.txt")
 
-    centroids = []
-    cent_coords = []
-    for sc in scanners:
-        coords, cent = centroid_scanner(sc)
-        centroids.append(cent)
-        cent_coords.append(coords)
+    all_beacons = [tuple(_) for _ in scanners[0]]
 
-    registry = register_points(cent_coords[1], cent_coords[0], 2)
-    toalign1 = np.vstack(list(registry.keys()))
-    toalign2 = np.vstack(list(registry.values()))
 
-    r, err = Rotation.align_vectors(toalign1, toalign2)
+    for ind,scanner in enumerate(scanners[1:]):
+        print("scanner: %d" % (ind + 1))
+        registry = register_points(scanner, all_beacons, 4)
+        print("registry length: %d" % len(registry))
+        if len(registry) > 0:
+            print(registry)
+            R, t = get_rotation_translation(registry)
+            translated_beacons = np.round(R.apply(scanner) + t,0).astype(np.int64)
+            print("translation: %s" % t)
+            for tb in translated_beacons:
+                tbtup = tuple(np.ravel(tb))
+                if tbtup not in all_beacons:
+                    all_beacons.append(tbtup)
+            print("all beacons length: %d" % len(all_beacons))
